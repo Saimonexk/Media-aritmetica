5e9cc1721c5ac54d41ac21d624bc9d56408e8339
Erick Gonçalves <120678046+Saimonexk@users.noreply.github.com>
2023-01-04 17:03:38 -0300

    Add files via upload

---
+++ Média aritmética.py
@@ -0,0 +1,5 @@
+# Média aritmética, ele vê a média de um número
+n1 = float(input('digite o primeiro número: '))
+n2 = float(input('digite um segundo número: '))
+m = (n1 + n2) /2
+print('a média desses números é {:.1f}'.format(m))
